71a069423e5b4943312538bf37b31440851dffd0
Adminstrator <1065090086@qq.com>
2019-11-06 19:10:02 +0800

    node-process-protect

---
+++ node/node-process-protect/fork-computer.js
@@ -0,0 +1,11 @@
+const forkComputer = () => {
+  let sum = 0;
+  for(let i = 0; i<10000000000; i++){
+    sum +=i;
+  }
+  return sum
+}
+process.on('message',msg=>{
+  const sum = forkComputer();
+  process.send(sum)
+})
